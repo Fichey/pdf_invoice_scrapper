a59fa28ceee55729e838ff12804e35aefcea507b
Cezary Polkowski <czarekpolk@gmail.com>
2025-08-20 14:19:33 +0200

    almost finished pdf scraping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pdfplumber
 import re
+import json
 
 
 TABLE_SETTINGS = {
@@ -22,6 +23,42 @@ def extract_tables_single_strategy(path: str):
         return tables
     return []
 
+def handle_tables(tables):
+    tables_by_type = {
+        "FedEx": []
+    }
+    for table in tables:
+        if table and table[0] == FEDEX_HEADER:
+            tables_by_type["FedEx"].append(table)
+
+    airtable_records = []
+
+    for type in tables_by_type:
+        if not tables_by_type[type]:
+            continue
+        if type == "FedEx":
+            for table in tables_by_type[type]:
+                result = handle_fedex_table(table)
+                if result:
+                    airtable_records.append({
+                        "fields": result
+                    })
+
+    return airtable_records
+
+
+def is_kg_between_newlines(text: str) -> bool:
+    return bool(re.search(r"\nkg\n", text, re.IGNORECASE))
+
+def is_there_reference_number(text: str) -> bool:
+    return bool(re.search(r"[()_]", text))
+
+def is_reference_number_between_newlines(text: str) -> bool:
+    return bool(re.search(r"\n\(\d+\)\n", text))
+
+def is_there_underscore_in_reference_number(text: str) -> bool:
+    return bool(re.search(r"[_]", text))
+
 def handle_fedex_table(table):
     if not table or len(table) < 2:
         return None
@@ -40,7 +77,100 @@ def handle_fedex_table(table):
         wymiary = rows[0][0].split('\n')[1].split('Wymiary')[1].split('cm')[0].strip().split('x')
         if len(wymiary) == 3:
             dlugosc, szerokosc, wysokosc = map(float, wymiary)
-    
+    usluga = rows[0][2].split('Waga')[0].replace('\n', ' ').strip()
+
+    match = re.search(r"([\d.,]+)", rows[0][2].split('Waga')[1])
+    waga_zafakturowana = float(match.group(1).replace(",", ".")) if match else None
+
+    dane = rows[0][3]
+    dane = dane.replace('.', '').replace(',', '.').replace('(PLN)', '')
+
+    kg_2_lines = is_kg_between_newlines(dane)
+    contains_reference_number = is_there_reference_number(dane)
+    contains_underscore = False
+    reference_number_in_2_lines = False
+    if contains_reference_number:
+        contains_underscore = is_there_underscore_in_reference_number(dane)
+        if not contains_underscore:
+            reference_number_in_2_lines = is_reference_number_between_newlines(dane)
+
+    dane = re.sub(r"\s+", " ", dane).strip()
+
+    sztuki = None
+    waga = None
+    numer_referencyjny = None
+    podlega_vat = None
+    bez_vat = None
+    lacznie = None
+
+    # print(f"kg_2_lines: {kg_2_lines}")
+    # tutaj przypisywanie tych danych da się zrobić bardziej elegancko, ale na razie zostawiam tak
+    dane_split = dane.split(' ')
+
+    if not kg_2_lines: # waga w jednej linii
+
+        if not contains_reference_number: # nie ma numeru referencyjnego
+            sztuki = int(dane_split[0])
+            waga = float(dane_split[1])
+            podlega_vat = float(dane_split[3])
+            bez_vat = float(dane_split[4])
+            lacznie = float(dane_split[5])
+
+        else: # jest numer referencyjny
+            podlega_vat = float(dane_split[4])
+            bez_vat = float(dane_split[5])
+            lacznie = float(dane_split[6])
+
+            if contains_underscore: # jest podkreślnik w numerze referencyjnym
+                sztuki = int(dane_split[1])
+                waga = float(dane_split[2])
+                numer_referencyjny = dane_split[0] + dane_split[7]
+
+            else:
+                if not reference_number_in_2_lines: # numer referencyjny w jednej linii
+                    sztuki = int(dane_split[0])
+                    waga = float(dane_split[1])
+                    numer_referencyjny = dane_split[3].replace("(", "").replace(")", "")
+                
+                else: # numer referencyjny w dwóch liniach
+                    sztuki = int(dane_split[1])
+                    waga = float(dane_split[2])
+                    numer_referencyjny = dane_split[7].replace("(", "").replace(")", "")
+
+    else: # waga w dwóch liniach
+
+        if not contains_reference_number: # nie ma numeru referencyjnego
+            sztuki = int(dane_split[1])
+            waga = float(dane_split[0])
+            podlega_vat = float(dane_split[2])
+            bez_vat = float(dane_split[3])
+            lacznie = float(dane_split[4])
+
+
+        # else: # jest numer referencyjny
+        #     podlega_vat = float(dane_split[4])
+        #     bez_vat = float(dane_split[5])
+        #     lacznie = float(dane_split[6])
+
+        #     if contains_underscore: # jest podkreślnik w numerze referencyjnym
+        #         sztuki = int(dane_split[0])
+        #         waga = float(dane_split[1])
+        #         numer_referencyjny = dane_split[3].replace('\n', '')
+
+        #     else:
+        #         if not reference_number_in_2_lines: # numer referencyjny w jednej linii
+        #             sztuki = int(dane_split[0])
+        #             waga = float(dane_split[1])
+        #             numer_referencyjny = dane_split[3].replace("(", "").replace(")", "")
+                
+        #         else: # numer referencyjny w dwóch liniach
+        #             sztuki = int(dane_split[1])
+        #             waga = float(dane_split[2])
+        #             numer_referencyjny = dane_split[7].replace("(", "").replace(")", "")
+
+
+
+    # print(f"Dane: {dane}")
     return {
         "typ": "FedEx",
         "AWB": AWB,
@@ -48,20 +178,28 @@ def handle_fedex_table(table):
         "dlugosc": dlugosc,
         "szerokosc": szerokosc,
         "wysokosc": wysokosc,
+        "usluga": usluga,
+        "waga_zafakturowana": waga_zafakturowana,
+        "sztuki": sztuki,
+        "waga": waga,
+        "numer_referencyjny": numer_referencyjny,
+        "podlega_vat": podlega_vat,
+        "bez_vat": bez_vat,
+        "lacznie": lacznie
     }
 
 
 
 def main():
-    path = "data/first_page.pdf"
+    path = "data/invoice1.pdf"
+    out_path = "data/records.json"
     tables = extract_tables_single_strategy(path)
-    fedex_tables = []
-    for table in tables:
-        if table and table[0] == FEDEX_HEADER:
-            fedex_tables.append(table)
-            for row in table:
-                print(row)
 
+    records = handle_tables(tables)
+
+
+    with open(out_path, "w", encoding="utf-8") as f:
+        json.dump(records, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == "__main__":
